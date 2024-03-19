9ef7651bdeb675d7290403f179a7df86134e2be2
chaubanban <bangcac24@gmail.com>
2024-03-19 09:44:11 +0700

    update1

---
+++ birthday-main/config.js
@@ -0,0 +1,32 @@
+
+var config = {
+   
+    texts: [
+        "Meo Hồng à!",    
+        "Chúc mừng sinh nhật bạn", 
+        "Tuổi mới hay ăn chóng lớn",
+        "Sức khoẻ dồi dào",
+        "Phát lộc phát tài",
+        "Vạn sự như ý",
+        "Có nhiều người yêu =)))",
+        "Thật nhiều hạnh phúc",
+        "Công việc suôn sẻ",
+        
+    ],
+
+    imgs: {
+        "Meo Hồng": "",
+  
+    },
+
+    desc: {
+        turn_on: "Bật đèn",
+        play: "Bật nhạc",
+        bannar_coming: "Hãy trang trí với tôi nào!!!",
+        balloons_flying: "Thêm bóng bay",
+        cake_fadein: "Thêm chiếc bánh ngon nhất từ trước đến nay",
+        light_candle: "Thêm nến nào",
+        wish_message: "Happy Birthday",
+        story: "Tôi có lời chúc đến bạn...",
+    }
+};
