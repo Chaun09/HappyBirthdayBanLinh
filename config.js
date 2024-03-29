834ae6140df031653bf7cece00fa5f3d91889bb8
chaubanban <bangcac24@gmail.com>
2024-03-29 22:21:58 +0700

    update11

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ var config = {
     desc: {
         turn_on: "Bật đèn",
         play: "Bật nhạc",
-        bannar_coming: "Hãy trang trí với tôi nào!!!",
+        bannar_coming: "Hãy trang trí với anh nào!!!",
         bannar_coming1:"Ảnh người đẹp nhất hôm nay xuất hiện",
         balloons_flying: "Thêm bóng bay",
         cake_fadein: "Thêm chiếc bánh ngon nhất từ trước đến nay",
