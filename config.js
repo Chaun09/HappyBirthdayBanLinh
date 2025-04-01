df9fc309e4be1a04109f47412428fa1c6e4680c7
Chaun09 <bangcac24@gmail.com>
2025-04-01 09:05:37 +0700

    oanh

--- config.js
+++ config.js
@@ -1,32 +1,27 @@
-
 var config = {
-   
-    texts: [
-        "Bạn Linh À",    
-        "Chúc mừng sinh nhật bạn", 
-        "Sức khoẻ dồi dào",
-        "Phát lộc phát tài",
-        "Vạn sự như ý",
-        "Sớm Đồng Ý Làm Người Yêu Tôi =))))))))))))))))))))))))))))))))))",
-        "Sớm đồng ý đi xem phim với tôi nữa nhé",
-        "Thật nhiều hạnh phúc",
-        "Công việc suôn sẻ",
-        "Thành Công Trên Con Đường Mình Đã Chọn Nhen"
-    ],
+  texts: [
+    "Bạn Oanh À",
+    "Chúc mừng sinh nhật bạn nho",
+    "Sức khoẻ dồi dào",
+    "Mãi xinh, mãi giỏi, mãi đỉnh hơn nho",
+    "Phát lộc phát tài",
+    "Vạn sự như ý",
+    "Bao giờ mà bạn rảnh bạn đi xem phim với tôi nho",
+    "Thật nhiều hạnh phúc",
+    "Công việc suôn sẻ",
+    "Mọi điều tốt đẹp sẽ đến với bạn nè",
+    "Thành Công Trên Con Đường Mình Đã Chọn nho",
+  ],
 
-    
-
-    desc: {
-        turn_on: "Bật đèn",
-        play: "Bật nhạc",
-        bannar_coming: "Hãy trang trí với tôi nào!!!",
-        bannar_coming1:"Ảnh người đẹp nhất hôm nay xuất hiện",
-        balloons_flying: "Thêm bóng bay",
-        cake_fadein: "Thêm chiếc bánh ngon nhất từ trước đến nay",
-        light_candle: "Thêm nến nào",
-        wish_message: "Happy Birthday",
-        story: "Tôi có lời chúc đến bạn...",
-    },
-
-   
+  desc: {
+    turn_on: "Bật đèn",
+    play: "Bật nhạc",
+    bannar_coming: "Hãy trang trí với tôi nào!!!",
+    bannar_coming1: "Ảnh người đẹp nhất hôm nay xuất hiện",
+    balloons_flying: "Thêm bóng bay",
+    cake_fadein: "Thêm chiếc bánh ngon nhất từ trước đến nay",
+    light_candle: "Thêm nến nào",
+    wish_message: "Happy Birthday",
+    story: "Tôi có lời chúc đến bạn...",
+  },
 };
